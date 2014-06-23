a8d2867848dc25aad51bc05005593867b8b53a17
skv-headless <skv-headless@yandex.ru>
2014-06-23 22:03:48 +0400

    remove projects

--- hub_watch.js
+++ hub_watch.js
@@ -12,6 +12,15 @@ if (Meteor.isClient) {
         Projects.insert({name: event.target.value})
         event.target.value = ''
       }
+    },
+    'mouseenter #projectList li': function (event) {
+      $(event.target).find('i').removeClass('hide')
+    },
+    'mouseleave #projectList li': function (event) {
+      $(event.target).find('i').addClass('hide')
+    },
+    'click #projectList li i.icon-cancel': function (event) {
+      Projects.remove(this._id)
     }
   });
 }
